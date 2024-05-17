2b10f8fc690f7e43edf8ffaa5d9c3b6b866513e9
StefchoK <ianakiev209@gmail.com>
2024-05-17 15:03:07 +0300

    Работещи ревюта (не точно)
    направих въвеждането на ревютата но не е завършено

---
+++ ReviewCode/reviewpage.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var writeReviewBtn = document.getElementById("RRB");
+    var reviewsContainer = document.getElementById("reviews");
+  
+    function displayStoredReviews() {
+      var storedReviews = localStorage.getItem("reviews");
+      if (storedReviews) {
+        storedReviews = JSON.parse(storedReviews);
+        storedReviews.forEach(function(review) {
+          createReviewElement(review);
+        });
+      }
+    }
+  
+    function saveReviewsToStorage(reviews) {
+      localStorage.setItem("reviews", JSON.stringify(reviews));
+    }
+  
+    function createReviewElement(reviewText) {
+      var reviewDiv = document.createElement("div");
+      reviewDiv.className = "review";
+      reviewDiv.innerHTML = "<h2>Ново Ревю</h2><p>" + reviewText + "</p>";
+      reviewsContainer.appendChild(reviewDiv);
+    }
+  
+    writeReviewBtn.addEventListener("click", function() {
+      var reviewText = prompt("Напишете вашето ревю:");
+      if (reviewText) {
+        createReviewElement(reviewText);
+        var storedReviews = localStorage.getItem("reviews");
+        if (storedReviews) {
+          storedReviews = JSON.parse(storedReviews);
+        } else {
+          storedReviews = [];
+        }
+        storedReviews.push(reviewText);
+        saveReviewsToStorage(storedReviews);
+      }
+    });
+  
+    displayStoredReviews();
+  });
+  
+  
+  
